489d120a5b77fe6b9e44f3cc918d4a8301ac4eb5
Jen Haines <motochick67@gmail.com>
2015-01-23 09:20:53 -0800

    Outgoing mail

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -4,8 +4,10 @@ if Rails.env.development?
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      ENV['SENDGRID_USERNAME'],
-    password:       ENV['SENDGRID_PASSWORD'],
+    user_name:      'app33291223@heroku.com',  
+    password:       'nqohixcu',
+    # user_name:      ENV['SENDGRID_USERNAME'],
+    # password:       ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150122010501) do
+ActiveRecord::Schema.define(version: 20150123051431) do
 
   create_table "apps", force: true do |t|
     t.string   "name"
@@ -51,6 +51,10 @@ ActiveRecord::Schema.define(version: 20150122010501) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
