4b2d965245576b0ea3dadba087c509dd9e2e8d5a
Jen Haines <motochick67@gmail.com>
2015-01-24 10:00:08 -0800

    mailer fixes

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,11 +1,11 @@
-if Rails.env.development?
+if Rails.env.development? || Rails.env.production?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     address:        'smtp.sendgrid.net',
     port:           '587',
     authentication: :plain,
-    user_name:      Rails.application.secrets.sendgrid_username,
-    password:      Rails.application.secrets.sendgrid_password,
+    user_name:      Rails.application.secrets.sendgrid_username ? Rails.env.development : ENV['SENDGRID_USERNAME'],
+    password:      Rails.application.secrets.sendgrid_password ? Rails.env.development : ENV['SENDGRID_PASSWORD'],
     domain:         'heroku.com',
     enable_starttls_auto: true
   }
